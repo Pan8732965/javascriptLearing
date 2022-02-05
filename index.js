703fa920ebb22a7480d4c0bb090d716d41ad4dc8
驄杰 潘 <smartjay1206@gmail.com>
2022-02-05 22:34:12 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//loading something in the browse console
+//console.log('hello world!');
+//---variables---
+let name= 'Jay';
+//console.log(name);
+
+// cannot be a reserved keyword
+//should be meaningful
+//cannot  start with a number(1name)
+//cannot contain a space or hyphen(-)
+//are case-sensitive(區分大小寫)
+
+let firstName = 'Pan';
+let lastName = 'Jay';
+
+console.log(firstName+lastName);
